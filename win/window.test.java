13515dc3f227bffe5a5183cdfffdc0a911fb2038
bobschlowinskiithecoder <120756894+bobschlowinskiithecoder@users.noreply.github.com>
2023-04-20 18:36:46 -0500

    dgsf

---
+++ win/window.test.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("hi");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+        System.out.println("Hi")
+        
+    }
+}

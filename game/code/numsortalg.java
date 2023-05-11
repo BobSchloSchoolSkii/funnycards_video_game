456016be1f1c6628b09507d72a7e19cba424f8f6
BobSchloSchoolSkii <131377036+BobSchloSchoolSkii@users.noreply.github.com>
2023-05-11 18:33:38 +0000

    minor_commit_1

---
+++ game/code/numsortalg.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class numsortalg {
+
+    public static void main(String[] args) {
+        double[] dat = {31,58,39259,1234567890,47520,1,354,-523};
+        Arrays.sort(dat);
+        System.out.println(Arrays.toString(dat));
+    }
+}
